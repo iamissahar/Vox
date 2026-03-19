30cd2cb9c8cad9d48e62cdec18ce4ec4d535dddb
l1qwie <programjibogdan@gmail.com>
2026-03-19 19:22:45 +0200

    asd

--- vox_web/src/App.tsx
+++ vox_web/src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './hooks/useAuth';
-import { ProtectedRoute } from './components/ProtectedRoute';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./hooks/useAuth";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
-import { HomePage } from './pages/HomePage';
-import { HostDashboardPage } from './pages/HostDashboardPage';
-import { HostBroadcastPage } from './pages/HostBroadcastPage';
-import { ListenerPage } from './pages/ListenerPage';
-import { ProfilePage } from './pages/ProfilePage';
+import { HomePage } from "./pages/HomePage";
+import { HostDashboardPage } from "./pages/HostDashboardPage";
+import { HostBroadcastPage } from "./pages/HostBroadcastPage";
+import { ListenerPage } from "./pages/ListenerPage";
+import { ProfilePage } from "./pages/ProfilePage";
 
-import './styles/globals.css';
+import "./styles/globals.css";
 
 export default function App() {
   return (
